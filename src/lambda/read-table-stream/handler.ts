36379c1908e484bf4c90b5162408f13120fcc71c
giuseppe.pace <giuseppe.pace@alten.it>
2021-06-27 19:26:15 +0200

    minor: log date on stream read

--- src/lambda/read-table-stream/handler.ts
+++ src/lambda/read-table-stream/handler.ts
@@ -5,7 +5,7 @@ let index: number = 0;
 
 export async function handler(event: DynamoDBStreamEvent, context: Context): Promise<any> {
   _.forEach(event.Records, record => {
-    console.log(index, record.dynamodb?.NewImage);
+    console.log(index, new Date().toISOString(), record.dynamodb?.NewImage);
     index++;
   })
 }
