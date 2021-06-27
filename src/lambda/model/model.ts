491f994bda976fbf096baab88536e0a5db44e427
giuseppe.pace <giuseppe.pace@alten.it>
2021-06-27 19:20:15 +0200

    feat: write multiple pk

--- src/lambda/model/model.ts
+++ src/lambda/model/model.ts
@@ -2,7 +2,8 @@ import { z } from "zod";
 
 
 export const writeLambdaInputSchema = z.object({
-  itemsToWrite: z.number().min(1).max(2000)
+  itemsToWrite: z.number().min(1).max(2000),
+  pkNumber: z.number().min(1).max(20).optional()
 })
 
 export type WriteLambdaInput = z.infer<typeof writeLambdaInputSchema>;
--- src/lambda/write-table/handler.ts
+++ src/lambda/write-table/handler.ts
@@ -10,10 +10,12 @@ const tableName = process.env.TABLE_NAME!;
 export async function handler(input: WriteLambdaInput, context: Context): Promise<any> {
   writeLambdaInputSchema.parse(input);
 
+  const pkNumber = input.pkNumber ?? 1;
 
   for (let i = 0; i < input.itemsToWrite; i++) {
+    const currentPkNumber = i % pkNumber;
     const item = {
-      pk: 'ITEM',
+      pk: `ITEM-${currentPkNumber}`,
       sk: i
     };
     await docClient.put({
